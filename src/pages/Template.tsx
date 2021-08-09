f05ac5af5aad6baf316cb03e577f5547dacb23d9
saathvikdm <saathvikdm@outlook.com>
2021-08-09 16:23:46 +0530

    pres

--- src/pages/Template.tsx
+++ src/pages/Template.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import footerLogo from '../../assets/footerlogo.png';
+
 export default function Template({
   paitentName,
   idNumber,
